5cf9779a69927a75ead02f267e401596b02eefa6
sycworkspace <yichen.sam.sun@hotmail.com>
2021-01-26 15:59:09 +0800

    Create 020_app.js

---
+++ 02 JavaScript Language Fundamentals/020_app.js
@@ -0,0 +1,40 @@
+// A Look At The Windows Object
+
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// Alert
+// alert('Hello World');
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if (confirm('Are you sure')) {
+//   console.log('YES');
+// } else {
+//   console.log('NO');
+// }
+
+let val;
+
+// Outter height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// Iutter height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll point
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+console.log(val);
