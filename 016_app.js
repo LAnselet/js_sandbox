ad61772c364c4df1a3c3483638a12eb40e3bd41e
sycworkspace <yichen.sam.sun@hotmail.com>
2021-01-26 14:53:20 +0800

    Create 016_app.js

---
+++ 016_app.js
@@ -0,0 +1,21 @@
+// if(something){
+//  do something
+// } else {
+//   do something else
+// }
+
+const id = 100;
+
+// EQUAL TO
+if (id == 101) {
+  console.log('CORRECT');
+} else {
+  console.log('INCORRECT');
+}
+
+// NOT EQUAL TO
+if (id != 101) {
+  console.log('CORRECT');
+} else {
+  console.log('INCORRECT');
+}
